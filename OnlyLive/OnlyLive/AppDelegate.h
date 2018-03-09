4389bfe9f8916cc69033325cd634e87871a29fa7
only <fengqiyunbian@sina.cn>
2018-03-09 11:26:04 +0800

    onlyLive
    onlyLive新建

---
+++ OnlyLive/OnlyLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlyLive
+//
+//  Created by only on 2018/3/9.
+//  Copyright © 2018年 only. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
